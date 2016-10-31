eb1a1d0a39099e22f6b2a1da5aeb688a13bf11e2
Raphael Oliveira <raphaelbrf@gmail.com>
2016-10-31 10:15:15 -0200

    Update some_functions.py

--- some_functions.py
+++ some_functions.py
@@ -26,3 +26,16 @@ print type('Spam')
 
 print type(True)
 
+# adicionando dados a um vetor
+suitcase = [] 
+suitcase.append("sunglasses")
+
+# Your code here!
+suitcase.append("chinello")
+suitcase.append("Toalha")
+suitcase.append("Touca")
+
+list_length = len(suitcase) # Set this to the length of suitcase
+
+print "There are %d items in the suitcase." % (list_length)
+print suitcase

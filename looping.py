8aa4717d73fd27cc88b126e812b8ad736da37fbd
Raphael Oliveira <raphaelbrf@gmail.com>
2016-11-08 16:16:01 -0200

    function zip

--- looping.py
+++ looping.py
@@ -54,7 +54,7 @@ for key in d:
     print print_this
 
 
-# print index with the key
+# function enumerate create index for keys
 
 choices = ['pizza', 'pasta', 'salad', 'nachos']
 
@@ -62,3 +62,16 @@ print 'Your choices are:'
 ###### enumarate() -> function create index for keys
 for index, item in enumerate(choices):
     print index, item
+    
+## function ZIP
+list_a = [3, 9, 17, 15, 19]
+list_b = [2, 4, 8, 10, 30, 40, 50, 60, 70, 80, 90]
+
+# zip will create pairs of elements when passed two lists, and will stop at the end of the shorter list.
+for a, b in zip(list_a, list_b):
+    # Add your code here!
+    if a > b:
+        print a
+    else:
+        print b
+    

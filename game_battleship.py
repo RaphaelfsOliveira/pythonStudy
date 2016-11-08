7b51db505b25b4ac52d0836dc094e8e9b6bddf5d
Raphael Oliveira <raphaelbrf@gmail.com>
2016-11-08 11:39:32 -0200

    Update game_battleship.py

--- game_battleship.py
+++ game_battleship.py
@@ -73,6 +73,7 @@ for turn in range(4):
     
     if guess_row == ship_row and guess_col == ship_col:
         print "Congratulations! You sunk my battleship!"
+        break
     else:
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col  > 4):
             print "Oops, that's not even in the ocean."
@@ -84,3 +85,5 @@ for turn in range(4):
     # Print (turn + 1) here!
     print_board(board)
     print "Turn", turn + 1
+    if turn == 3:
+    print "Game Over"

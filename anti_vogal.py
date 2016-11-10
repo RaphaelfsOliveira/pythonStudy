cb04833100a3e70fe3735b1f14d00e7a69c918ec
Raphael Oliveira <raphaelbrf@gmail.com>
2016-11-10 15:33:12 -0200

    create anti_vowel.py

---
+++ anti_vogal.py
@@ -0,0 +1,22 @@
+def anti_vowel(text):
+    t = ""
+
+    for c in text:
+        boolean = False
+
+        #print text[c]
+        for v in "aeiouAEIOU":
+            
+            if c == v:
+                boolean = True
+                print "True: ",c, v
+            else:
+                print "False: ",c, v
+                
+        #print "Check final: ",boolean 
+        if not boolean:
+            t += c
+        
+    return t
+
+print anti_vowel("Raphael")

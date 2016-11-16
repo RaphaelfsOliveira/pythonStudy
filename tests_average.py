c64f958ea913a09cfd862883735d8dc8c8931f77
Raphael Oliveira <raphaelbrf@gmail.com>
2016-11-16 13:11:54 -0200

    update

--- tests_average.py
+++ tests_average.py
@@ -4,13 +4,25 @@ def print_grades(grades):
     for i in grades:
         print i
 
+        
 def grades_sum(grades):
     total = 0
     for i in grades: 
         total += i
     return total
     
+    
 def grades_average(grades):
     return grades_sum(grades) / float(len(grades))
     
-
+    
+def grades_variance(scores):
+    average = grades_average(scores)
+    variance = 0
+    
+    for i in scores:
+        variance += (average - i) ** 2
+    return variance / float(len(scores))
+    
+    
+print grades_variance(grades)

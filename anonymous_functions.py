a03133ba5e8702dd13977f0d9d5de01da6e6fbcd
Raphael Oliveira <raphaelbrf@gmail.com>
2016-11-17 15:16:39 -0200

    Create  anonymous_functions.py

---
+++ anonymous_functions.py
@@ -0,0 +1,20 @@
+####functional programming
+"""
+One of the more powerful aspects of Python
+is that it allows for a style of programming 
+called functional programming, which means that 
+you're allowed to pass functions around just as 
+if they were variables or values. Sometimes we 
+take this for granted, but not all languages allow this!
+"""
+
+my_list = range(16)
+
+print filter(lambda x: x % 3 == 0, my_list)
+
+
+languages = ["HTML", "JavaScript", "Python", "Ruby"]
+print filter(lambda i: i == "Python", languages)
+
+squares = [x**2 for x in range(1,11)]
+print filter(lambda x: x > 30 and x <= 70, squares)

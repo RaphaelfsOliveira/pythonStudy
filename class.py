45057acd8158b780c1fc9135fcc3a8288376b6be
Raphael Oliveira <raphaelbrf@gmail.com>
2016-11-19 13:42:45 -0200

    Update class.py

--- class.py
+++ class.py
@@ -129,4 +129,30 @@ print Jack.employee_name, Jack.calculate_wage(10)
 
 #print Employee.calculate_wage(6)
 
-            
+
+#################Inheritance !!
+class Car(object):
+    condition = "new"
+    
+    def __init__(self, model, color, mpg):
+        self.model = model
+        self.color = color
+        self.mpg   = mpg
+    
+    def display_car(self):
+        return "This is a "+ self.color +" "+ self.model +\
+        " with "+ str(self.mpg) +" MPG."
+    
+    def drive_car(self):
+        self.condition = "used"
+
+class ElectricCar(Car):
+    #################Inheritance !!
+    def __init__(self, model, color, mpg, battery_type):
+        Car.__init__(self, model, color, mpg)
+        self.battery_type = battery_type
+           
+my_car = ElectricCar("DeLorean", "silver", 88, "molten salt")
+
+
+
